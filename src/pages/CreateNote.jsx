11a4caa3f7ace747a2821ff1b6f4b5d4ae7d7e5f
aditya-narla <adi.nar@outlook.com>
2026-04-22 11:11:35 -0400

    Added placeholder pages

---
+++ src/pages/CreateNote.jsx
@@ -0,0 +1,9 @@
+function CreateNote() {
+    return (
+        <main>
+            <h1>Create Note</h1>
+        </main>
+    )
+}
+
+export default CreateNote
---
+++ src/pages/EditNote.jsx
@@ -0,0 +1,9 @@
+function EditNote() {
+    return (
+        <main>
+            <h1>Edit Note</h1>
+        </main>
+    )
+}
+
+export default EditNote
