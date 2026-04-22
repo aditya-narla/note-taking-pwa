b9b8ebc031b61c5790fcbfc90f47f1cfe43b9b01
aditya-narla <adi.nar@outlook.com>
2026-04-22 17:42:43 -0400

    Added function for addNote

--- src/db/db.js
+++ src/db/db.js
@@ -15,3 +15,15 @@ function openDB() {
         }
     })
 }
+
+export async function addNote(note) {
+    const db = await openDB()
+    return new Promise((resolve, reject) => {
+        const tx = db.transaction('notes', 'readwrite')
+        const store = tx.objectStore('notes')
+        const request = store.add(note)
+
+        request.onsuccess = () => resolve(request.result)
+        request.onerror = () => reject(request.error)
+    })
+}
