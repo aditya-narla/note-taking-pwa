cdbb7f322976b80ab4643a2829a40da86d50b503
aditya-narla <adi.nar@outlook.com>
2026-04-23 15:09:04 -0400

    Added method to update note

--- src/db/db.js
+++ src/db/db.js
@@ -60,3 +60,18 @@ export async function getNoteById(id) {
         request.onerror = () => reject(request.error)
     })
 }
+
+export async function updateNote(note) {
+
+    const db = await openDB()
+
+    return new Promise((resolve, reject) => {
+
+        const tx = db.transaction('notes', 'readwrite')
+        const store = tx.objectStore('notes')
+        const request = store.put(note)
+
+        request.onsuccess = () => resolve(request.result)
+        request.onerror = () => reject(request.error)
+    })
+}
