573045de5c0795c7966fcc4643887a31d0f74ae6
aditya-narla <adi.nar@outlook.com>
2026-04-23 14:22:32 -0400

    Added method to get note by ID

--- src/db/db.js
+++ src/db/db.js
@@ -17,11 +17,44 @@ function openDB() {
 }
 
 export async function addNote(note) {
+
     const db = await openDB()
+
     return new Promise((resolve, reject) => {
+
         const tx = db.transaction('notes', 'readwrite')
         const store = tx.objectStore('notes')
         const request = store.add(note)
+
+        request.onsuccess = () => resolve(request.result)
+        request.onerror = () => reject(request.error)
+    })
+}
+
+export async function getAllNotes() {
+
+    const db = await openDB()
+
+    return new Promise((resolve, reject) => {
+
+        const tx = db.transaction('notes', 'readonly')
+        const store = tx.objectStore('notes')
+        const request = store.getAll()
+
+        request.onsuccess = () => resolve(request.result)
+        request.onerror = () => reject(request.error)
+    })
+}
+
+export async function getNoteById(id) {
+
+    const db = await openDB()
+
+    return new Promise((resolve, reject) => {
+
+        const tx = db.transaction('notes', 'readonly')
+        const store = tx.objectStore('notes')
+        const request = store.get(id)
 
         request.onsuccess = () => resolve(request.result)
         request.onerror = () => reject(request.error)
