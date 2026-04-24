1488f34698f0d75e6434465d6fa91b5ed4af07f6
aditya-narla <adi.nar@outlook.com>
2026-04-24 16:04:07 -0400

    Built page for creating note

--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react"
+import { useNotes } from "../hooks/useNotes";
+import { useNavigate } from "react-router-dom";
+
 function CreateNote() {
+
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const { add } = useNotes();
+    const navigate = useNavigate();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        add({ title, content });
+        navigate('/');
+    }
+
     return (
         <main>
             <h1>Create Note</h1>
+
+            <form onSubmit={handleSubmit}>
+
+                <label>Title</label>
+                <input type="text" placeholder="Title"
+                value={title} onChange={e => setTitle(e.target.value)} />
+
+                <label>Content</label>
+                <textarea value={content} 
+                onChange={e => setContent(e.target.value)}></textarea>
+
+                <button type="submit">Save</button>
+
+            </form>
         </main>
     )
 }
