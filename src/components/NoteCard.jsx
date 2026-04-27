936cd7ab84dfc44e1977ad95474e9c2ef90592b7
aditya-narla <adi.nar@outlook.com>
2026-04-27 17:31:30 -0400

    Added styling for homepage

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,13 +1,18 @@
+import { Link } from "react-router-dom"
 import { formatDate } from "../utils/formatDate"
 
 function NoteCard({ note }) {
     return (
-        <div className="note-card">
-            <h3> {note.title} </h3>
-            <p> {note.content} </p>
-            <p> {note.createdAt && formatDate(note.createdAt)} </p>
+        <Link to={`/edit/${note.id}`} className="note-card">
             {note.photo && <img src={note.photo} alt="note photo" />}
-        </div>
+            <div>
+                <div className="note-card-header">
+                    <h3> {note.title} </h3>
+                    <p> {note.createdAt && formatDate(note.createdAt)} </p>
+                </div>
+                <p className="note-content"> {note.content} </p>
+            </div>
+        </Link>
     )
 }
 
