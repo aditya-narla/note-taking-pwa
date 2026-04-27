b9965877539181079f02e8860c246524852aad71
aditya-narla <adi.nar@outlook.com>
2026-04-27 12:19:56 -0400

    Built EditNote to edit notes and save

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -1,7 +1,48 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { getNoteById } from "../db/db";
+import { useNotes } from "../hooks/useNotes";
+
 function EditNote() {
+
+    const { id } = useParams();
+
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const { update } = useNotes();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        async function loadNote() {
+            const note = await getNoteById(Number(id));
+            setTitle(note.title);
+            setContent(note.content);
+        }
+        loadNote();
+    }, [id]);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        update({ id: Number(id), title, content});
+        navigate('/');
+    }
+
     return (
         <main>
             <h1>Edit Note</h1>
+
+            <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input type="text" placeholder="Title"
+                value={title} onChange={e => setTitle(e.target.value)} />
+
+                <label>Content</label>
+                <textarea value={content}
+                onChange={e => setContent(e.target.value)}></textarea>
+
+                <button type="submit">Save</button>
+            </form>
         </main>
     )
 }
